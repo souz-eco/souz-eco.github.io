2535c499feadffff536c8bb6ac23b6683ff5e07d
vlad1777d <naumovvladislav@mail.ru>
2017-04-26 02:11:13 +0300

    Added Yandex Metrika.

--- commit and export.py
+++ commit and export.py
@@ -4,7 +4,7 @@
 import os
 
 
-os.system('hg add *')
+os.system('hg addremove')
 os.system('hg commit')
 os.system('hg push git+ssh://git@github.com:souz-eco/souz-eco.github.io.git')
 print("All operations done.")
